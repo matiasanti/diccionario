86c5dcf70ca9836e094df394d4f1ab89bbfeb22a
matiasanti <matiasanti2050@gmail.com>
2025-01-13 18:22:23 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "ROFL": "una respuesta a una broma",
+            "SHEESH": "ligera desaprobación",
+            "CREEPY": "aterrador, siniestro",
+            "AGGRO": "ponerse agresivo/enojado"
+            }
+
+print("Bienvenidos al diccionario de palabras modernas")
+
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys(5):
+    # ¿Qué debemos hacer si se encuentra la palabra?
+    print(meme_dict[word])
+else:
+    print("Esta palabra no esta en el diccionario")
